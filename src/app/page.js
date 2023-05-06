b0822c714e31b9849539b732d24b68b25a69dd45
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-05-06 17:51:06 -0500

    added fonts and name

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,15 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        Hello World!
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-900">
+      <div className="mb-32 grid text-center text-3xl lg:mb-0 lg:grid-cols-4 lg:text-left"> 
+        <div className='font-mono'>
+          Hello,
+          My name is
+        </div>
+        <div className='font-anton font-bold'>
+          Koby Sillito
+        </div>
       </div>
     </main>
   )
