b627a3c3ab2cbbcde26b3a621d9d35969f9110ea
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-05-24 20:07:14 -0500

    added portrait and social links

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,49 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
-import lumiel from "./images/lumiel.jpg";
-import texas from "./images/texas.png";
+import Profile from "./images/profile.jpg";
+import Lumiel from "./images/lumiel.jpg";
+import Texas from "./images/texas.png";
 import Form from './components/form.js'
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillFileText,
+  FiMapPin,
+} from "react-icons/ai";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 bg-slate-900">
       <section id="#top" className="justify-between">
-        <div className="mb-32 flex-column text-center text-3xl w-full items-center ">
+        <div className="mb-12 flex-column text-center text-3xl w-full items-center ">
           <div className="font-mono">Hello! My name is</div>
           <div className="font-anton font-bold rounded-xl w-3/4 p-2 justify-center bg-slate-600">Koby Sillito</div>
           <Image
             className="rounded-full mt-10"
-            src={lumiel}
+            src={Lumiel}
             alt="profile picture"
             width={300}
             height={300}
           />
         </div>
+        {/* Socials */}
+        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <a href="https://github.com/KobyS82" target="_blank">
+              <AiFillGithub className="hover:text-pink-400"/>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/koby-sillito/"
+              target="_blank"
+            >
+              <AiFillLinkedin className="hover:text-pink-400"/>
+            </a>
+            <a
+              href="https://docs.google.com/document/d/17N3A6zOqL7IJ6HCXbcePu9OvhL8HGvxpBMXVfIZhKWc/edit?usp=sharing"
+              target="_blank"
+            >
+              <AiFillFileText className="hover:text-pink-400"/>
+            </a>
+          </div>
       </section>
       <section id="#about" className="">
         <div className="mb-32 grid text-center text-base justify-items-center">
@@ -29,7 +54,7 @@ export default function Home() {
           <div className="bg-slate-600 p-5 rounded-xl">
             <Image
               className=""
-              src={texas}
+              src={Texas}
               alt="University of Texas picture"
               width={300}
               height={300}
