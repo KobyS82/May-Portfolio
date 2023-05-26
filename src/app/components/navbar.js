f836e5df3148c5dc9496dd1c7cd3eba3a30824b2
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-05-25 19:02:22 -0500

    pages aren't working, navbar updated

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -3,23 +3,31 @@ import React from "react";
 import Link from "next/link";
 const Navbar = () => {
   return (
-    <nav className='flex items-center flex-wrap p-3 bg-slate-900'>
-      <div className="inline-flex flex-grow w-auto">
-        <ul className="inline-flex flex-row ml-auto w-full justify-evenly h-auto text-cyan-200">
-          <li className="w-full px-3 py-2 rounded items-center justify-center hover:text-white hover:font-bold hover:underline hover:underline-offset-auto">
-            <Link href="#top">Top</Link>
-          </li>
-          <li className="w-full px-3 py-2 rounded items-center justify-center hover:text-white hover:font-bold hover:underline hover:underline-offset-auto">
-            <Link href="#about">About</Link>
-          </li>
-          <li className="w-full px-3 py-2 rounded items-center justify-center hover:text-white hover:font-bold hover:underline hover:underline-offset-auto">
-            <Link href="#work">Work</Link>
-          </li>
-          <li className="w-full px-3 py-2 rounded items-center justify-center hover:text-white hover:font-bold hover:underline hover:underline-offset-auto">
-            <Link href="#contact">Contact</Link>
-          </li>
-        </ul>
-      </div>
+    <nav className="p-3 w-full bg-slate-900 flex justify-evenly text-cyan-200">
+        <Link
+          className="px-3 py-2 rounded items-center hover:text-white hover:font-bold hover:underline hover:underline-offset-auto"
+          href="/"
+        >
+          Top
+        </Link>
+        <Link
+          className="px-3 py-2 rounded items-center hover:text-white hover:font-bold hover:underline hover:underline-offset-auto"
+          href="/about"
+        >
+          About
+        </Link>
+        <Link
+          className="px-3 py-2 rounded items-center hover:text-white hover:font-bold hover:underline hover:underline-offset-auto"
+          href="#work"
+        >
+          Work
+        </Link>
+        <Link
+          className="px-3 py-2 rounded items-center hover:text-white hover:font-bold hover:underline hover:underline-offset-auto"
+          href="#contact"
+        >
+          Contact
+        </Link>
     </nav>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -45,26 +45,7 @@ export default function Home() {
             </a>
           </div>
       </section>
-      <section id="#about" className="">
-        <div className="mb-32 grid text-center text-base justify-items-center">
-          <h2 className="text-3xl justify-items-start">About me!</h2>
-          <p className="w-72 pb-10 text-neutral-400">
-            I am a full stack web developer with a passion for making webpages and learning new things.
-          </p>
-          <div className="bg-slate-600 p-5 rounded-xl">
-            <Image
-              className=""
-              src={Texas}
-              alt="University of Texas picture"
-              width={300}
-              height={300}
-            />
-            <p className="w-72 text-white">
-              I graduated from the University of Texas at Austin with a certificate in Full Stack Web Development.
-            </p>
-          </div>
-        </div>
-      </section>
+      
       <section id="#work">
         <h2>work!</h2>
       </section>
